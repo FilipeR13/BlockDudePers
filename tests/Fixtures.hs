0316e1376b3da326856631b52ba800999f51bab2
Jose_R_Rodrigues <a100692@alunos.uminho.pt>
2021-11-28 22:59:51 +0000

    Adicionei mais alguns mapas

--- tests/Fixtures.hs
+++ tests/Fixtures.hs
@@ -68,6 +68,87 @@ m2 =
     (Caixa, (10,4)),
     (Caixa, (14,4)) 
   ]
+m3 :: [(Peca, Coordenadas)]
+m3 = [(Porta, (0,2)), 
+      (Bloco, (0,3)),
+      (Bloco, (1,3)),
+      (Bloco, (2,3)),
+      (Bloco, (3,3)),
+      (Caixa, (4,2)),
+      (Bloco, (4,3)),
+      (Bloco, (5,3)),
+      (Bloco, (6,0)),
+      (Bloco, (6,1)),
+      (Bloco, (6,2)),
+      (Bloco, (6,3))
+    ]
+m3' :: [(Peca,Coordenadas)]
+m3' = [(Porta, (0,2)), 
+      (Bloco, (0,3)),
+      (Bloco, (1,3)),
+      (Bloco, (2,3)),
+      (Bloco, (3,3)),
+      (Caixa, (4,1)),
+      (Bloco, (4,3)),
+      (Bloco, (5,3)),
+      (Bloco, (6,0)),
+      (Bloco, (6,1)),
+      (Bloco, (6,2)),
+      (Bloco, (6,3))
+    ]
+m4 :: [(Peca, Coordenadas)] 
+m4 = 
+  [ (Porta, (1,4)),
+    (Bloco, (0,0)),
+    (Bloco, (0,1)),
+    (Bloco, (0,2)),
+    (Bloco, (0,3)),
+    (Bloco, (0,4)),
+    (Bloco, (0,5)),
+    (Bloco, (1,5)),
+    (Bloco, (2,5)),
+    (Bloco, (3,5)),
+    (Bloco, (3,2)),
+    (Bloco, (4,5)),
+    (Bloco, (5,5)),
+    (Bloco, (6,6)),
+    (Bloco, (7,5)), 
+    (Bloco, (8,5)),
+    (Bloco, (9,5)),
+    (Bloco, (10,5)),
+    (Bloco, (11,5)),
+    (Bloco, (12,5)),
+    (Bloco, (13,5)),           
+    (Bloco, (14,5)),
+    (Bloco, (15,5)),
+    (Bloco, (16,5)),
+    (Bloco, (17,5)),
+    (Bloco, (18,5)),
+    (Bloco, (19,5)),
+    (Bloco, (3,4)),
+    (Bloco, (3,3)),
+    (Bloco, (8,4)),
+    (Caixa, (10,4)),
+    (Bloco, (12,4)),
+    (Bloco, (12,3)),
+    (Caixa, (14,4)),
+    (Bloco, (5,6)),
+    (Bloco, (7,6)),
+    (Caixa, (4,4))
+  ]
+m5 :: [(Peca,Coordenadas)]
+m5 = 
+  [ (Bloco,(0,0)),
+    (Porta,(0,1)),
+    (Bloco,(1,0)),
+    (Bloco,(1,1)),
+    (Bloco,(0,2)),
+    (Bloco,(1,2)),
+    (Bloco,(2,2)),
+    (Bloco,(2,1)),
+    (Bloco,(2,0))
+  ]
+
 
 m1r :: Mapa
 m1r =
@@ -89,6 +170,16 @@ m2r =
     [Vazio,Vazio,Vazio,Vazio,Vazio,Bloco,Bloco,Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio]
   ]
 
+m3r :: Mapa 
+m3r = 
+  [ [Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio],
+    [Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio],
+    [Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio],
+    [Bloco,Vazio,Vazio,Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio],
+    [Bloco,Porta,Vazio,Bloco,Vazio,Vazio,Vazio,Vazio,Bloco,Vazio,Caixa,Vazio,Vazio,Vazio,Caixa,Vazio,Vazio,Vazio,Vazio,Vazio],
+    [Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Vazio,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco]
+  ]
+
 m1e1 :: Jogo
 m1e1 = Jogo m1r (Jogador (6, 0) Oeste False)
 
@@ -97,3 +188,6 @@ m1e2 = Jogo m1r (Jogador (2, 3) Oeste False)
 
 m1e3 :: Jogo
 m1e3 = Jogo m2r (Jogador (2, 2) Este False)
+
+m1e4 :: Jogo
+m1e4 = Jogo m2r (Jogador (17,4) Este False)
