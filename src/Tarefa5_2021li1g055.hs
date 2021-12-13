3812b872033add8bb77f7d2d68112392c54428aa
a100642 <a100642@alunos.uminho.pt>
2021-12-13 17:25:01 +0000

    Criei menu com opção de jogar um nível

--- src/Tarefa5_2021li1g055.hs
+++ src/Tarefa5_2021li1g055.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {- | 
 Module      : Tarefa5_2021li1g055
 Description : Aplicação Gráfica
@@ -12,19 +13,30 @@ import Tarefa2_2021li1g055
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
+data Opcoes = Jogar 
+            | Sair
 
+data Menu = Controlador Opcoes
+          | ModoJogo Jogo
+          | VenceuJogo
 
-type Estado =Jogo
+type Estado = Menu
 
 transformaJogo :: Estado -> Picture
-transformaJogo ((Jogo j (Jogador (x,y) b d))) = translate adjustx adjusty (pictures $ transformaJogoAux (desconstroiMapa j) (Jogador (x,y) b d)) 
+transformaJogo (Controlador Jogar) = Pictures [Color blue $ drawOption "Jogar", Translate (0) (-70) $ drawOption "Sair"]
+transformaJogo (Controlador Sair) = Pictures [drawOption "Jogar",color blue $ Translate (0) (-70) $ drawOption "Sair"]
+transformaJogo (ModoJogo (Jogo j (Jogador (x,y) b d))) = translate adjustx adjusty (pictures $ transformaJogoAux (desconstroiMapa j) (Jogador (x,y) b d))                                              
                                               where adjusty = -32 * fromIntegral (ymax (desconstroiMapa j)) / 2 
                                                     adjustx = -32 * fromIntegral (xmax (desconstroiMapa j)) / 2
 transformaJogoAux :: [(Peca,Coordenadas)] -> Jogador -> [Picture]
-transformaJogoAux [] (Jogador (x,y) dir b) = [translate z w jogador]
-      where  jogador = rectangleSolid 32 32 
+transformaJogoAux [] (Jogador (x,y) dir b) = case dir of Este -> if b then [translate z w jogadorE, translate z w' caixa] else [translate z w jogadorE]
+                                                         Oeste -> if b then [translate z w jogadorO, translate z w' caixa] else [translate z w jogadorO]      
+      where  jogadorE = color yellow $ rectangleSolid 32 32
+             jogadorO = rectangleSolid 32 32 
+             caixa = color blue $ rectangleSolid 32 32
              z = 32 * fromIntegral x
              w = 32 * fromIntegral (-y)
+             w' = 32 * fromIntegral (-(y-1))
 transformaJogoAux m@((p,(a,b)):t) (Jogador (x,y) dir s)
                                  | p == Bloco = translate c d bloco : transformaJogoAux t (Jogador (x,y) dir s) 
                                  | p == Porta = translate c d porta : transformaJogoAux t (Jogador (x,y) dir s) 
@@ -36,7 +48,11 @@ transformaJogoAux m@((p,(a,b)):t) (Jogador (x,y) dir s)
          c = 32 * fromIntegral a
          d = 32 * fromIntegral (-b)
 
--- "Estados"GlossInicial
+drawOption option = Translate (-50) 0 $ Scale (0.5) (0.5) $ Text option
+
+estadoGlossInicial :: Estado 
+estadoGlossInicial = Controlador Jogar
+
 nivel1 :: Jogo
 nivel1 = 
   Jogo   [ [Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio],
@@ -51,6 +67,17 @@ desenhaEstadoGloss :: Estado -> Picture
 desenhaEstadoGloss = transformaJogo
 
 reageEventoGloss :: Event -> Estado -> Estado
+reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Jogar) = ModoJogo nivel1 
+reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Jogar) =Controlador Sair
+reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Jogar) = Controlador Sair
+reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Sair) =Controlador Jogar
+reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Sair) = Controlador Jogar
+reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Sair) = undefined
+
+reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (ModoJogo j) =ModoJogo (moveJogador j Trepar)
+reageEventoGloss  (EventKey (SpecialKey KeyDown) Down _ _) (ModoJogo j) =ModoJogo (moveJogador j InterageCaixa)
+reageEventoGloss  (EventKey (SpecialKey KeyRight) Down _ _) (ModoJogo j) =ModoJogo (moveJogador j AndarDireita)
+reageEventoGloss  (EventKey (SpecialKey KeyLeft) Down _ _) (ModoJogo j) =ModoJogo (moveJogador j AndarEsquerda)
 reageEventoGloss _ j = j 
 
 reageTempoGloss:: Float -> Estado -> Estado
@@ -64,4 +91,4 @@ dm = InWindow "BlocK Dude" (1000,1000) (0,0)
 
 
 main :: IO ()
-main = play dm (greyN 0.8) fr nivel1 desenhaEstadoGloss reageEventoGloss reageTempoGloss
+main = play dm (greyN 0.8) fr estadoGlossInicial desenhaEstadoGloss reageEventoGloss reageTempoGloss
