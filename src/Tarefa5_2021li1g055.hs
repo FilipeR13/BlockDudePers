0d5b351f9e456628570b10a5ec28d374078c33bf
Jose_R_Rodrigues <a100692@alunos.uminho.pt>
2021-12-21 21:45:06 +0000

    Introduzi as imagens default no jogo

--- src/Tarefa5_2021li1g055.hs
+++ src/Tarefa5_2021li1g055.hs
@@ -12,6 +12,7 @@ import Tarefa4_2021li1g055
 import Tarefa2_2021li1g055
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
+import Data.Maybe
 
 data Opcoes = Jogar 
             | Sair
@@ -24,41 +25,35 @@ data Menu = Controlador Opcoes
           | ModoJogo Jogo
           | VenceuJogo
 
-type Estado = (Menu,Int)
+type Estado = (Menu,Int,([(Peca,Picture)],(Picture,Picture)))
 
 transformaJogo :: Estado -> Picture
-transformaJogo (Controlador Jogar,_) = Pictures [Color blue $ drawOption "Jogar", Translate (0) (-70) $ drawOption "Sair"]
-transformaJogo (Controlador Sair,_) = Pictures [drawOption "Jogar",color blue $ Translate (0) (-70) $ drawOption "Sair"]
-transformaJogo (Nivel Nivel1,_) = Pictures [Color blue $ drawOption "Nivel 1", Translate (0) (-70) $ drawOption "Nivel 2"]
-transformaJogo (Nivel Nivel2,_) = Pictures [drawOption "Nivel 1",color blue $ Translate (0) (-70) $ drawOption "Nivel 2"]
-transformaJogo (VenceuJogo,2) = Translate (-200) 0 $ Color red $ Text "Ganhou"
-transformaJogo (ModoJogo (Jogo j (Jogador (x,y) b d)),_) = translate adjustx adjusty (pictures $ transformaJogoAux (desconstroiMapa j) (Jogador (x,y) b d))                                              
+transformaJogo (Controlador Jogar,_,_) = Pictures [Color blue $ drawOption "Jogar", Translate (0) (-70) $ drawOption "Sair"]
+transformaJogo (Controlador Sair,_,_) = Pictures [drawOption "Jogar",color blue $ Translate (0) (-70) $ drawOption "Sair"]
+transformaJogo (Nivel Nivel1,_,_) = Pictures [Color blue $ drawOption "Nivel 1", Translate (0) (-70) $ drawOption "Nivel 2"]
+transformaJogo (Nivel Nivel2,_,_) = Pictures [drawOption "Nivel 1",color blue $ Translate (0) (-70) $ drawOption "Nivel 2"]
+transformaJogo (VenceuJogo,2,_) = Translate (-200) 0 $ Color red $ Text "Ganhou"
+transformaJogo (ModoJogo (Jogo j (Jogador (x,y) b d)),_,l) = translate adjustx adjusty (pictures $ transformaJogoAux (desconstroiMapa j) (Jogador (x,y) b d) l)                                              
                                               where adjusty = -32 * fromIntegral (ymax (desconstroiMapa j)) / 2 
                                                     adjustx = -32 * fromIntegral (xmax (desconstroiMapa j)) / 2
-transformaJogoAux :: [(Peca,Coordenadas)] -> Jogador -> [Picture]
-transformaJogoAux [] (Jogador (x,y) dir b) = case dir of Este -> if b then [translate z w jogadorE, translate z w' caixa] else [translate z w jogadorE]
-                                                         Oeste -> if b then [translate z w jogadorO, translate z w' caixa] else [translate z w jogadorO]      
-      where  jogadorE = color yellow $ rectangleSolid 32 32
-             jogadorO = rectangleSolid 32 32 
-             caixa = color blue $ rectangleSolid 32 32
-             z = 32 * fromIntegral x
+transformaJogoAux :: [(Peca,Coordenadas)] -> Jogador -> ([(Peca,Picture)],(Picture,Picture)) -> [Picture]
+transformaJogoAux [] (Jogador (x,y) dir b) l = case dir of Este -> if b then [translate z w (snd (snd l)), translate z w' (fromJust (lookup Caixa (fst l)))] else [translate z w (snd (snd l))]
+                                                           Oeste -> if b then [translate z w (fst (snd l)), translate z w' (fromJust (lookup Caixa (fst l)))] else [translate z w (fst (snd l))]    
+      where  z = 32 * fromIntegral x
              w = 32 * fromIntegral (-y)
              w' = 32 * fromIntegral (-(y-1))
-transformaJogoAux m@((p,(a,b)):t) (Jogador (x,y) dir s)
-                                 | p == Bloco = translate c d bloco : transformaJogoAux t (Jogador (x,y) dir s) 
-                                 | p == Porta = translate c d porta : transformaJogoAux t (Jogador (x,y) dir s) 
-                                 | p == Caixa = translate c d caixa : transformaJogoAux t (Jogador (x,y) dir s) 
-                                 | otherwise = transformaJogoAux t (Jogador (x,y) dir s) 
-   where bloco = color red $ rectangleSolid 32 32
-         porta = color green $ rectangleSolid 32 32
-         caixa = color blue $ rectangleSolid 32 32
-         c = 32 * fromIntegral a
+transformaJogoAux m@((p,(a,b)):t) (Jogador (x,y) dir s) l
+                                 | p == Bloco = translate c d (fromJust (lookup Bloco (fst l))) : transformaJogoAux t (Jogador (x,y) dir s) l
+                                 | p == Porta = translate c d (fromJust (lookup Porta (fst l))) : transformaJogoAux t (Jogador (x,y) dir s) l
+                                 | p == Caixa = translate c d (fromJust (lookup Caixa (fst l))) : transformaJogoAux t (Jogador (x,y) dir s) l
+                                 | otherwise = transformaJogoAux t (Jogador (x,y) dir s) l
+   where c = 32 * fromIntegral a
          d = 32 * fromIntegral (-b)
 
 drawOption option = Translate (-50) 0 $ Scale (0.5) (0.5) $ Text option
 
-estadoGlossInicial :: Estado 
-estadoGlossInicial = (Controlador Jogar,0)
+estadoGlossInicial :: ([(Peca,Picture)],(Picture,Picture)) -> Estado 
+estadoGlossInicial l = (Controlador Jogar,0,l)
 
 nivel1 :: Jogo
 nivel1 = 
@@ -84,30 +79,31 @@ desenhaEstadoGloss :: Estado -> Picture
 desenhaEstadoGloss = transformaJogo
 
 reageEventoGloss :: Event -> Estado -> Estado
-reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Jogar,0) = (Nivel Nivel1,0)
-reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Jogar,0) = (Controlador Sair,0)
-reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Jogar,0) = (Controlador Sair,0)
-reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Sair,0) = (Controlador Jogar,0)
-reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Sair,0) = (Controlador Jogar,0)
-reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Sair,0) = undefined
+reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Jogar,0,l) = (Nivel Nivel1,0,l) --
+reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Jogar,0,l) =( Controlador Sair,0,l)
+reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Jogar,0,l) = (Controlador Sair,0,l)
+reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Sair,0,l) =(Controlador Jogar,0,l)
+reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Sair,0,l) = (Controlador Jogar,0,l)
+reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Sair,0,l) = undefined
 
-reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Nivel Nivel1,0) = (Nivel Nivel1,0)
-reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Nivel Nivel1,0) = (Nivel Nivel2,0)
-reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Nivel Nivel2,0) = (Nivel Nivel1,0)
-reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Nivel Nivel2,0) = (Nivel Nivel1,0)
-reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Nivel Nivel1,0) = (ModoJogo nivel1,1)
-reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Nivel Nivel2,0) = (ModoJogo nivel2,2)
+reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Nivel Nivel1,0,l) =( Nivel Nivel2 ,0,l)
+reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Nivel Nivel1,0,l) = (Nivel Nivel2,0,l)
+reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (Nivel Nivel2,0,l) =( Nivel Nivel1,0,l)
+reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (Nivel Nivel2,0,l) = (Nivel Nivel1,0,l)
+reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Nivel Nivel1,0,l) =(ModoJogo nivel1,1,l)
+reageEventoGloss (EventKey (SpecialKey KeyEnter) Down _ _) (Nivel Nivel2,0,l) = (ModoJogo nivel2,2,l)
 
-reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (ModoJogo j,n) = (ModoJogo (moveJogador j Trepar),n)
-reageEventoGloss  (EventKey (SpecialKey KeyDown) Down _ _) (ModoJogo j,n) = (ModoJogo (moveJogador j InterageCaixa),n)
-reageEventoGloss  (EventKey (SpecialKey KeyRight) Down _ _) (ModoJogo j,n) =(ModoJogo (moveJogador j AndarDireita),n)
-reageEventoGloss  (EventKey (SpecialKey KeyLeft) Down _ _) (ModoJogo j,n) = (ModoJogo (moveJogador j AndarEsquerda),n)
-reageEventoGloss (EventKey r Down _ _) (ModoJogo j,n) | n == 1 = (ModoJogo nivel1,n) 
-                                                      | n == 2 = (ModoJogo nivel2,n)
+reageEventoGloss  (EventKey (SpecialKey KeyUp) Down _ _) (ModoJogo j,n,l) = (ModoJogo (moveJogador j Trepar),n,l)
+reageEventoGloss   (EventKey (SpecialKey KeyDown) Down _ _) (ModoJogo j,n,l) =( ModoJogo (moveJogador j InterageCaixa),n,l)
+reageEventoGloss  (EventKey (SpecialKey KeyRight) Down _ _) (ModoJogo j,n,l) = (ModoJogo (moveJogador j AndarDireita),n,l)
+reageEventoGloss  (EventKey (SpecialKey KeyLeft) Down _ _) (ModoJogo j,n,l) =(ModoJogo (moveJogador j AndarEsquerda),n,l)
+--
+reageEventoGloss (EventKey (Char 'r') Down _ _) (ModoJogo j,n,l) | n == 1 = (ModoJogo nivel1,n,l) 
+                                                        | n == 2 = (ModoJogo nivel2,n,l)
 
-reageEventoGloss _ (ModoJogo v@((Jogo j (Jogador (x,y) b d))), n) | elem (Porta,(x,y)) (desconstroiMapa j) && n == 1 = (ModoJogo nivel2,2)
-                                                                  | elem (Porta,(x,y)) (desconstroiMapa j) && n == 2 = (VenceuJogo,2)
-                                                                  |otherwise = (ModoJogo v,n)
+reageEventoGloss _ (ModoJogo v@((Jogo j (Jogador (x,y) b d))), n,l) | elem (Porta,(x,y)) (desconstroiMapa j) && n == 1 = (ModoJogo nivel2,2,l)
+                                                                  | elem (Porta,(x,y)) (desconstroiMapa j) && n == 2 = (VenceuJogo,2,l)
+                                                                  |otherwise = (ModoJogo v,n,l)
 reageEventoGloss _ j = j 
 
 reageTempoGloss:: Float -> Estado -> Estado
@@ -121,4 +117,10 @@ dm = InWindow "BlocK Dude" (1000,1000) (0,0)
 
 
 main :: IO ()
-main = play dm (greyN 0.8) fr estadoGlossInicial desenhaEstadoGloss reageEventoGloss reageTempoGloss
+main = do
+  caixa <- loadBMP "caixaDefault.bmp"
+  porta <- loadBMP "portadefault.bmp"
+  bloco <- loadBMP "BlockDefault.bmp" 
+  bonecoD <- loadBMP "jogadorDD.bmp"
+  bonecoE <- loadBMP "jogadorDE.bmp"
+  play dm (greyN 0.8) fr (estadoGlossInicial ([(Bloco,bloco),(Caixa,caixa),(Porta,porta)],(bonecoE,bonecoD))) desenhaEstadoGloss reageEventoGloss reageTempoGloss
