bfb8b30af8645f50d8777afcbf830e2149d97a26
a100642 <a100642@alunos.uminho.pt>
2022-01-03 11:41:34 +0000

    Adicionei copyright e apaguei a linha conflituosa posta pelo editor

--- src/Tarefa5_2021li1g055.hs
+++ src/Tarefa5_2021li1g055.hs
@@ -1,8 +1,9 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {- | 
 Module      : Tarefa5_2021li1g055
 Description : Aplicação Gráfica
-
+Copyright   : Lucas Quintela <a100642@alunos.uminho.pt>;
+            : José Rodrigues <a100692@alunos.uminho.pt>;
+            
 Módulo para a realização da Tarefa 5 do projeto de LI1 em 2021/22.
 -}
 module Main where
